870bd0a1e776b2d648974b431896233fb5f8b5b3
Mukul Ghanghas <partimag55@gmail.com>
2023-04-03 19:40:24 +0530

    modified

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 const button1 = document.getElementById("password");
 const passGen = () => {
-  for (let i = 0; i <= str.length; i++) {
-    var number = Math.floor(Math.random() * str.length + 1);
-    password = str.substring(number, number + 1);
+  for (let i = 0; i <= 5; i++) {
+    password += str.charAt(Math.floor(Math.random() * str.length + 1));
   }
-
+  // return password;
   document.getElementById("password").value = password;
 };
+
 function copyPass() {
   var copyText = document.getElementById("password");
 
   copyText.select();
   document.execCommand("copy");
+  document.getElementById("password").value = "";
 }
 copyPass();
-// const copys = document.querySelector(".copy1");
-// const copyPass = () => {
-//   copys.classList.add("copy");
-// };
-// const icons = document.querySelector("#copy");
-// const iconPass = () => {
-//   icons.classList.remove("copy");
-// };
-let str = "!@$%^&*()_+}{][]}'?/.,:|AQWERTYUIOPLKJJHGFDSAZXCVBNM";
+
+let str = "!@$%^&*()_+}{][]}'?/.,:AQWERTYUIOPLKJJHGFDSAZXCVBNM";
 var password = "";
